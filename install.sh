f534c9736a83d9d52a6542e2064fe42df5e099a5
Zaedus <nintendozaedus@gmail.com>
2022-10-14 14:05:42 -0500

    Add an install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+INSTALL_DIR="/usr/share/gtkui/"
+
+error() {
+  printf " Failed.\n"
+  if [ "$UID" != "0" ]; then
+    echo "Error: You must run this script as root!"
+  fi
+  exit
+}
+
+if [ "$1" != "-r" ]; then
+  printf "Installing GtkUI libraries..."
+  if [ ! -d "$INSTALL_DIR" ]; then
+    mkdir "$INSTALL_DIR" &> /dev/null || error
+  fi
+
+  cp *.gui "$INSTALL_DIR" &> /dev/null || error
+else
+  printf "Removing all GtkUI libraries..."
+  rm -rf /usr/share/gtkui/ &> /dev/null || error
+fi
+
+printf " Done!\n"
